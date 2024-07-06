a78f78ee99b4bd8269a1fbbb1e58830f307f3f6a
Jordan Miles <j@jmiles42.com>
2024-07-06 23:54:48 +1000

    Add script to get and edit manifest file

---
+++ scripts/EditManifestFromGitHub.ps1
@@ -0,0 +1,30 @@
+ param (
+     [string]$pluginRepoUrl,
+     [string]$pluginZipName,
+     [string]$pluginGuid,
+     [string]$filename,
+     [string]$version,
+     [string]$manifestUrl,
+     [string]$targetAbi = '10.9.0.0',
+     [string]$manifestFileName = "manifest.json",
+     [string]$changelog = 'Auto Released by Actions'
+ )
+
+ $manifestJson = Invoke-RestMethod -Uri $manifestUrl;
+
+ $fileHash = Get-FileHash $filename -Algorithm MD5;
+ $url = "$pluginRepoUrl/releases/download/v$version/$pluginZipName@v$version.zip";
+ $newVersionEntry = @{
+     checksum   = $fileHash
+     changelog  = $changelog
+     targetAbi  = $targetAbi
+     sourceUrl  = $url
+     timestamp  = (Get-Date).ToString("yyyy-MM-ddTHH:mm:ssZ")
+     version    = $version
+ };
+
+ $val = $manifestJson[0] | where-object { $_.guid -eq $pluginGuid }
+ $val.versions.Insert(0, $newVersionEntry);
+
+ $manifestJson | ConvertTo-Json -Depth 10 | Set-Content -Path $manfestFileName;
+

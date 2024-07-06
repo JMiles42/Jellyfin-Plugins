1693c031437a75424fb07070d1724ffd87e6cb48
Jordan Miles <j@jmiles42.com>
2024-07-07 00:26:54 +1000

    Fixed typo in manifestFileName

--- scripts/EditManifestFromGitHub.ps1
+++ scripts/EditManifestFromGitHub.ps1
@@ -26,5 +26,5 @@
  $val = $manifestJson[0] | where-object { $_.guid -eq $pluginGuid }
  $val.versions = @($newVersionEntry) + $val.versions;
 
- $manifestJson | ConvertTo-Json -Depth 10 | Set-Content -Path $manfestFileName;
+ $manifestJson | ConvertTo-Json -Depth 10 | Set-Content -Path $manifestFileName;
 
